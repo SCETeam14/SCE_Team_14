10a53772a9e5fa7b99548318451167339a7d961e
arielchernis <44745531+arielchernis@users.noreply.github.com>
2020-04-14 20:08:40 +0300

    Update addNumbers.test.js

--- tests/addNumbers.test.js
+++ tests/addNumbers.test.js
@@ -1,5 +1,5 @@
 
-const addNumbers = require('../../src/addNumbers.js');
+const addNumbers = require('../../tests/addNumbers.js');
 
 describe('Test my add function', () => {
   it('add two positive numbers', () => {
