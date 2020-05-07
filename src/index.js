558c8a8be794fffbdc6d4b094e3af358a979acc3
arielchernis <44745531+arielchernis@users.noreply.github.com>
2020-05-07 17:35:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app_port = process.env.PORT || 3000
 const app = express()
-app.get('/', (req, res) => {
- res.send('Hello World')
+app.set('view engine','HTML')
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 })
 app.listen(app_port)
 console.log(`app is running. port: ${app_port}`)
