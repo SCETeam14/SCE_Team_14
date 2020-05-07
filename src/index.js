15a61060fb9562542cd7f0ab5476937be7bf44e9
arielchernis <44745531+arielchernis@users.noreply.github.com>
2020-05-07 18:01:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-var express = require('express');
-var app = express();
+onst express = require('express')
+const app_port = process.env.PORT || 3000
+const app = express()
 
-document.write("Super-sami");
-});
+document.write("<h1>Super-sami!</h1>");
+document.write("<hr/>");
+
+app.listen(app_port)
+console.log(`app is running. port: ${app_port}`)
+console.log(`http://127.0.0.1:${app_port}/`)
