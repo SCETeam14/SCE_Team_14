039e073f2ab14fe165738ed7ead9ec89cb2e5fdb
arielchernis <44745531+arielchernis@users.noreply.github.com>
2020-05-07 19:49:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
 var http = require('http');
+
+//create a server object:
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!'); //write a response to the client
+  res.write('samiii!'); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
 
