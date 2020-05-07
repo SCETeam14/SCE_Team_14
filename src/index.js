ab353237e274ca974e632000317f62c2ee439f5b
arielchernis <44745531+arielchernis@users.noreply.github.com>
2020-05-07 19:55:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 var http = require('http');
+var fs = require('fs');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('samiii!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
-
-
+http.createServer(function(req, res){
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(8000);
