7c0183023fca128c00f30d3782d723b49651d816
arielchernis <44745531+arielchernis@users.noreply.github.com>
2020-05-08 15:22:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const app_port = process.env.PORT || 3000
-const app = express()
-app.get('/', (req, res) => {
- res.send('Welcome To Super-Sami')
-})
+var http = require('http'),
+    fs = require('fs');
 
-app.listen(app_port)
-console.log(`app is running. port: ${app_port}`)
-console.log(`http://127.0.0.1:${app_port}/`)
+
+fs.readFile('./index1.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
