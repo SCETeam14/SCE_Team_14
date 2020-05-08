6be08f32a33ac76d2ba5bdbe2e535cfe209b92df
arielchernis <44745531+arielchernis@users.noreply.github.com>
2020-05-08 14:47:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ app.get('/', (req, res) => {
  res.send('Welcome To Super-Sami')
 })
 
-app.get('/signup', (req, res) => {
- res.send('signup here')
-})
 app.listen(app_port)
 console.log(`app is running. port: ${app_port}`)
 console.log(`http://127.0.0.1:${app_port}/`)
