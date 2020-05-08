7252a13752d19b369ba8052ca1686dc742765aa5
arielchernis <44745531+arielchernis@users.noreply.github.com>
2020-05-08 14:44:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ app.get('/', (req, res) => {
  res.send('Hello World')
 })
 app.listen(app_port)
-console.log(`app is running. port: ${app_port}
+console.log(`app is running. port: ${app_port}`)
 console.log(`http://127.0.0.1:${app_port}/`)
