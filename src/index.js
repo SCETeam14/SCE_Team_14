9f0adb54e0a30b09505beed0dfdc5a18a3103483
arielchernis <44745531+arielchernis@users.noreply.github.com>
2020-05-10 17:24:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-const app_port = process.env.PORT || 5000
-var http = require('http'),
-    fs = require('fs');
 
-
-fs.readFile('./src/index2.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(process.env.PORT || 5000)
-    
-});
